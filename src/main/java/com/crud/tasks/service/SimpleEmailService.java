10ade1c41572e0640450dc6d256405b5f9b1c99f
Małgorzata Galera <malgorzata.galera.92@gmail.com>
2020-04-24 02:56:20 +0200

    Add toCc in Mail

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -32,6 +32,10 @@ public class SimpleEmailService {
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
+        if (mail.getToCc() != null && !mail.getToCc().isEmpty()) {
+            mailMessage.setCc(mail.getToCc());
+            LOGGER.info("Set two mail receivers");
+        }
         return mailMessage;
     }
 }
--- src/test/java/com/crud/tasks/service/SimpleEmailServiceTestSuite.java
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTestSuite.java
@@ -24,7 +24,24 @@ public class SimpleEmailServiceTestSuite {
     @Test
     public void shouldSendEmail() {
         //Given
-        Mail mail = new Mail("test@test.com", "test", "teeeeeeessssstttttttttt");
+        Mail mail = new Mail("test@test.com", "test2@test.com", "test", "teeeeeeessssstttttttttt");
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setCc(mail.getToCc());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+
+        //When
+        simpleEmailService.send(mail);
+
+        //Then
+        verify(javaMailSender, times(1)).send(mailMessage);
+    }
+
+    @Test
+    public void shouldSendEmailWithOneReceiver() {
+        //Given
+        Mail mail = Mail.builder().mailTo("two@test.com").subject("second").message("message").build();
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
