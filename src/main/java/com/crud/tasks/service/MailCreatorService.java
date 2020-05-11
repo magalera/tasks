b719047e12c43c86b5251b04a5a0639e6d67f01c
Małgorzata Galera <malgorzata.galera.92@gmail.com>
2020-05-11 04:00:16 +0200

    Add conditional statements

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
 
@@ -18,6 +21,12 @@ public class MailCreatorService {
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello account");
+        functionality.add("Application allows sending tasks to Trello");
+        
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://magalera.github.io/");
@@ -30,6 +39,10 @@ public class MailCreatorService {
         context.setVariable("company_owner_email", adminConfig.getCompanyEmail());
         context.setVariable("company_owner_address_street", adminConfig.getCompanyAddressStreet());
         context.setVariable("company_owner_address_number", adminConfig.getCompanyAddressStreetNumber());
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", true);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/created-trello-card-mail.html", context);
     }
 }
