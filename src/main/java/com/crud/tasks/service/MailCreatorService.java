2298b0ab7bf1fbd6d9af1efec54ac2033337b988
Małgorzata Galera <malgorzata.galera.92@gmail.com>
2020-05-12 16:15:32 +0200

    32.3 Add tasks-information-mail.html mail template

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,6 +1,8 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -19,15 +21,17 @@ public class MailCreatorService {
     @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
+    @Autowired
+    private TaskRepository taskRepository;
 
     public String buildTrelloCardEmail(String message) {
 
+        Context context = new Context();
         List<String> functionality = new ArrayList<>();
         functionality.add("You can manage your tasks");
         functionality.add("Provides connection with Trello account");
         functionality.add("Application allows sending tasks to Trello");
-        
-        Context context = new Context();
+
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://magalera.github.io/");
         context.setVariable("button", "Visit website");
@@ -44,5 +48,27 @@ public class MailCreatorService {
         context.setVariable("admin_config", adminConfig);
         context.setVariable("application_functionality", functionality);
         return templateEngine.process("created-trello-card-mail.html", context);
+    }
+
+    public String tasksInformationEmail(String message) {
+        List<Task> tasks = taskRepository.findAll();
+
+        Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "http://magalera.github.io/");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("admin_mail", adminConfig.getAdminMail());
+        context.setVariable("mail_subject", adminConfig.getMailSubject());
+        context.setVariable("company_owner_name", adminConfig.getCompanyOwnerName());
+        context.setVariable("company_owner_surname", adminConfig.getCompanyOwnerSurname());
+        context.setVariable("company_owner_email", adminConfig.getCompanyEmail());
+        context.setVariable("company_owner_address_street", adminConfig.getCompanyAddressStreet());
+        context.setVariable("company_owner_address_number", adminConfig.getCompanyAddressStreetNumber());
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", true);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("tasks", tasks);
+        return templateEngine.process("tasks-information-mail.html", context);
     }
 }
